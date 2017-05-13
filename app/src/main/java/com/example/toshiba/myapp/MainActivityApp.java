576bc30a476d9fa52b4e4d510b72908d3f9f790b
zeligowska <michalina.zeligowska@gmail.com>
2017-05-14 00:08:36 +0200

    commit

---
+++ app/src/main/java/com/example/toshiba/myapp/MainActivityApp.java
@@ -0,0 +1,44 @@
+package com.example.toshiba.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivityApp extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_app);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("Wciśnięto przycisk1");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("Wciśnięto przycisk2");
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("Wciśnięto przycisk3");
+            }
+        });
+
+    }
+
+
+
+}

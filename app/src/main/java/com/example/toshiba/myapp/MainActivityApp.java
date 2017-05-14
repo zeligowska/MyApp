fb921270dcbdf26dcc63aab3d29cb22421117939
zeligowska <michalina.zeligowska@gmail.com>
2017-05-14 22:58:37 +0200

    commit

--- app/src/main/java/com/example/toshiba/myapp/MainActivityApp.java
+++ app/src/main/java/com/example/toshiba/myapp/MainActivityApp.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static com.example.toshiba.myapp.R.id.firstName;
+import static com.example.toshiba.myapp.R.id.lastName;
+
 public class MainActivityApp extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,8 @@ public class MainActivityApp extends Activity {
         Button button1 = (Button) findViewById(R.id.button1);
         Button button2 = (Button) findViewById(R.id.button2);
         Button button3 = (Button) findViewById(R.id.button3);
+        Button buttonSubmit = (Button) findViewById(R.id.buttonSubmit);
+
 
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -34,6 +39,13 @@ public class MainActivityApp extends Activity {
             public void onClick(View v) {
                 TextView textView = (TextView) findViewById(R.id.textView);
                 textView.setText("Wciśnięto przycisk3");
+            }
+        });
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.textView2);
+                textView.setText("dsvcvxvx");
             }
         });
 

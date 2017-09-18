bd1f7f6a77598c289b7b23adb05570b7e9e50715
zeligowska <michalina.zeligowska@gmail.com>
2017-09-18 21:12:29 +0200

    photo

--- app/src/main/java/com/example/toshiba/myapp/MainActivityApp.java
+++ app/src/main/java/com/example/toshiba/myapp/MainActivityApp.java
@@ -7,7 +7,10 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +19,15 @@ import android.widget.Toast;
 
 import com.example.toshiba.myapp.phones.Phones;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivityApp extends Activity {
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
+    private int fileUri;
+    private int fileUri2;
 
     private MediaPlayer mediaPlayer;
 
@@ -61,6 +72,19 @@ public class MainActivityApp extends Activity {
         mediaPlayer.stop();
     }
 
+    public void makePhoto(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File imagesFolder = new File(Environment.getExternalStorageDirectory(),
+                "MyImages");
+        imagesFolder.mkdirs();
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File image = new File(imagesFolder, "tutorial_" + timeStamp + ".jpg");
+        Uri uriSavedImage = Uri.fromFile(image);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,6 +99,7 @@ public class MainActivityApp extends Activity {
         Button buttonSubmit = (Button) findViewById(R.id.buttonSubmit);
         Button buttonPhones = (Button) findViewById(R.id.buttonPhones);
         Button btnNewAlertDialogButton = (Button) findViewById(R.id.btnNewAlertDialogButton);
+        Button photoButton = (Button) findViewById(R.id.photoButton);
 
 
         button1.setOnClickListener(new View.OnClickListener() {
@@ -123,6 +148,10 @@ public class MainActivityApp extends Activity {
             }
         });
 
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                makePhoto(v);
+            }
+        });
     }
-
 }
